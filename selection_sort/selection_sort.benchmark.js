ff03b2a73e8cabddb7d4adc76f28a466326ab3a5
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-08-29 15:15:24 +0200

    Working benchmark

--- selection_sort/selection_sort.benchmark.js
+++ selection_sort/selection_sort.benchmark.js
@@ -2,8 +2,8 @@ const selection_sort = require('./selection_sort');
 var Benchmark = require('../benchmark.js/benchmark');
 var suite = new Benchmark.Suite;
 
-function randomArray() {
-  var randArray = Array.from(Array(5).keys()).shift();
+function randomArray(len) {
+  var randArray = Array.from(Array(len).keys());
 
   var shuffle = function() {
     for (var i = randArray.length - 1; i > 0; i--) {
@@ -16,22 +16,18 @@ function randomArray() {
   return randArray
 }
 
-// var rand50 = randomArray(50);
-// var rand1mil = randomArray(1000000);
+var rand50 = randomArray(50);
+var rand100k = randomArray(100000);
 
 
-// suite.add('Selection sort with 50 elements', function() { 
-//   return selection_sort(rand50);
-// })
-// .add('Selection sort with 1000000 elements', function() { 
-//   return selection_sort(rand1mil);
-// })
-// .on('cycle', function(event) {
-//   console.log(String(event.target));
-// })
-// .on('complete', function() {
-//   console.log('Fastest is ' + this.filter('fastest').map('name'));
-// })
-// .run({'async': true});
+suite.add('Selection sort with 50 elements', function() { 
+  return selection_sort(rand50);
+})
+.add('Selection sort with 1000000 elements', function() { 
+  return selection_sort(rand100k);
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.run({'async': true});
 
-// console.log(randomArray())
