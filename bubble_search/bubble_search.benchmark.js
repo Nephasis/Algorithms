3ad868b33df7a6f974609f9291adcbcf8fb579fa
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-08-29 14:31:46 +0200

    Added gitignore

---
+++ bubble_search/bubble_search.benchmark.js
@@ -0,0 +1,28 @@
+const bubble_search = require('./bubble_search');
+var Benchmark = require('../benchmark.js/benchmark');
+var suite = new Benchmark.Suite;
+
+function randomArray(len) {
+  var randArray = Array.from(Array(len).keys()).shift();
+
+  var shuffle = function() {
+    for (var i = randArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = randArray[i];
+      randArray[i] = randArray[j];
+      randArray[j] = temp;
+    } 
+  }();
+  return randArray
+}
+
+suite.add('Selection sort with 50 elements', function() { 
+  return bubble_search(randomArray(50));
+})
+.add('Selection sort with 100000 elements', function() { 
+  return bubble_search(randomArray(100000));
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.run({'async': true});
--- bubble_search/bubble_search.test.js
+++ bubble_search/bubble_search.test.js
@@ -3,24 +3,6 @@ const bubble_search = require('./bubble_search');
 var unsorted = [2, 6, 1, 7, 4, 9];
 var sorted = [1, 2, 4, 6, 7, 9];
 
-var bigSortedArray = Array.from(Array(100000).keys())
-
-function shuffle(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  } 
-  return array;
-}
-
-var bigUnsortedArray = shuffle(bigSortedArray);
-
 test('Given an unordered list, order items in increasing order', () => {
     expect(JSON.stringify(bubble_search(unsorted))).toBe(JSON.stringify(sorted));
-})
-
-test('Given a big unordered list of 100.000 elements, order items in increasing order', () => {
-    expect(JSON.stringify(bubble_search(bigUnsortedArray))).toBe(JSON.stringify(bigSortedArray));
 })
