be222c1947c7a53ee6d2b602490140bb49662fde
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-08-29 12:52:43 +0200

    Fixed comments, names and added a benchmark test for 100 000 elements

--- bubble_search/bubble_search.js
+++ bubble_search/bubble_search.js
@@ -1,21 +1,17 @@
 function bubble_search(list) {
-	// keep looping until all passes in the step would be without any change
-	var cond = true;
-	while (cond) {
-		cond = false;
+	var changeInStepHappened = true;
+	while (changeInStepHappened) {
+		changeInStepHappened = false;
 		for (var i = 0; i < list.length-1; i++) {
 			if (list[i] > list[i+1]) {
 				var temp = list[i];
 				list[i] = list[i+1];
 				list[i+1] = temp;
-				cond = true;
+				changeInStepHappened = true;
 			}
 		}
 	}
 	return list;
 }
-
-// var unsorted = [2, 6, 1, 7, 4, 9];
-// bubble_search(unsorted);
 
 module.exports = bubble_search
